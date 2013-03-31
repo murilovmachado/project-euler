3d80c748d95c1d938ba2b0c2b1712c406a8e2afc
Murilo Machado <murilovmachado@gmail.com>
2013-03-31 00:20:22 -0700

    Solved problem 004.

---
+++ problem_004.rb
@@ -0,0 +1,46 @@
+def getLargestPalindromeFromProductOfTwoFactors(digitsFromFactors)
+	maxFactor = ("9" * digitsFromFactors).to_i
+	palidromes = Array.new
+	count = 0
+
+	# Optimization variables
+	optimizingOn = false
+	maxFirstFactorForPalindrome = 0
+
+	i = maxFactor
+	while i >= 1 and i > maxFirstFactorForPalindrome
+		j = i
+		while j >= 1
+			count += 1
+			product = i * j
+			if isPalindrome product
+				palidromes.push product
+				if !optimizingOn
+					optimizingOn = true
+					maxFirstFactorForPalindrome = j
+				end
+				break
+			end
+			j -= 1
+		end
+		i -= 1
+	end
+
+	palidromes.max
+end
+
+def isPalindrome(number)
+	result = true
+	numberString = number.to_s
+
+	for i in 0..(numberString.size / 2)
+		if numberString[i] != numberString[- (i + 1)]
+			result = false
+		end
+	end
+
+	result
+end
+
+answer = getLargestPalindromeFromProductOfTwoFactors(3)
+puts answer
